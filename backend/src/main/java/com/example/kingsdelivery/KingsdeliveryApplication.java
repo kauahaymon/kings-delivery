93fd3b75fbef48ce686e814b2eff1b9d65beae20
kauahaymon <kauahaymon@hotmail.com>
2024-07-01 15:21:10 -0400

    Project created!

---
+++ backend/src/main/java/com/example/kingsdelivery/KingsdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.kingsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingsdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KingsdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/kingsdelivery/KingsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kingsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
